fb7c31aa4ec4977b366e4680e073e7887e815f8e
Garvit Joshi <joshigarvit51467@gmail.com>
2026-07-24 01:39:00 +0530

    LC 56 merge intervals solution added

--- 56.merge-intervals.cpp
+++ 56.merge-intervals.cpp
@@ -1,10 +1,28 @@
-
 class Solution {
 public:
-    vector<vector<int> merge(vector<vector<int>& intervals) {
-                    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //sorting the intervals
+        sort(intervals.begin(),intervals.end());
+        //answer array created
+        vector<vector<int>> ans;
+        int currentstart = intervals[0][0];
+        int currentend = intervals[0][1];
+        for (int i = 1;i<intervals.size();i++){
+            //if overlapping
+            if (currentend>=intervals[i][0]){
+                currentend = max(currentend,intervals[i][1]);
+                
+                
+            }
+            //if not overlapping
+            else{
+                ans.push_back({currentstart,currentend});
+                currentend = intervals[i][1]; 
+                currentstart = intervals[i][0];
 
+            }
+        }
+        ans.push_back({currentstart,currentend});
+        return ans;
     }
 };
-
-
