71f880cdcff469e989b15859a1dbbdde4accd307
Garvit Joshi <joshigarvit51467@gmail.com>
2026-08-13 22:56:40 +0530

    LC3731 Sollution Added

--- arrays/3731-find-missing-elements.cpp
+++ arrays/3731-find-missing-elements.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        unordered_set<int> s;
-        int minimum = *min_element(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         int maximum = *max_element(nums.begin(),nums.end());
+        int minimum = *min_element(nums.begin(),nums.end());
+        int j = 0;
+        vector<int> ans;
 
-        for (int i = minimum;i<maximum;i++){
-            s.insert(i);
-        }
-        for(int j =0;j<nums.size();j++){
-            if(s.count(nums[j])){
-                
+        for (int i=minimum; i<=maximum; i++){
+            if(j<nums.size()&&nums[j] == i){
+                j++;
             }
-
+            else{
+                ans.push_back(i);
+            }
         }
+        return ans;
     }
 };
