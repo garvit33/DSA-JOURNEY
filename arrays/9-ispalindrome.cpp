bfa8ed33c4161fff94e6db0dacf6676486a2a5c5
Garvit Joshi <joshigarvit51467@gmail.com>
2026-08-25 00:54:00 +0530

    LC 9 Sollution Added:

---
+++ arrays/9-ispalindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long reverse = 0;
+        int digit = 0;
+        int original = x;
+        while(original>0){
+            digit = original%10;
+            original = original/10;
+            reverse = reverse*10+digit;
+        }
+        if(x == reverse){
+            return true;
+        }
+        return false;
+    }
+};
