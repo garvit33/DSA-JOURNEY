d4adaa26013b84ea4e28c00bf3af114a83de0b54
Garvit Joshi <joshigarvit51467@gmail.com>
2026-09-21 23:00:48 +0530

    LC 18  Sollution Added

---
+++ two pointer/18-4SUM.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i+3<nums.size();i++){
+            //skip duplicate values for i 
+            if(i>0 && nums[i] == nums[i-1])continue;
+
+            for(int j = i+1;j+2<nums.size();j++){
+                //skip duplicate value for j 
+               if(j > i+1 && nums[j] == nums[j-1])continue;
+                
+            int left = j+1;
+            int right = nums.size()-1;
+
+            while(left<right){
+                long long sum = (long long)nums[i]+nums[j]+nums[left]+nums[right];
+
+                if(sum<target){
+                    left++;
+                }
+                else if(sum>target){
+                    right--;
+                }
+                //push if 4 elements sum == target 
+                else{
+                    ans.push_back({
+                        nums[i],
+                        nums[j],
+                        nums[left],
+                        nums[right],
+                    });
+                    //handles duplicates for left
+                    while(left<right && nums[left] == nums[left+1]){
+                        left++;
+                    }
+                    //handles dups for right
+                    while(left<right && nums[right] == nums[right-1]){
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                }
+                } 
+            }
+        }
+        return ans;
+    }
+};
